bec8c8aa9054fe95114fa9a5980535fb3e8c7497
justin1732 <57789046+justin1732@users.noreply.github.com>
2020-04-21 12:07:10 -0500

    First Step

---
+++ data/friends.js
@@ -0,0 +1,73 @@
+var friends = [
+{
+    name:"Actual Cannibal Shia LeBeouf",
+    photo: "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAJBwU6.img?h=0&w=720&m=6&q=60&u=t&o=f&l=f&x=1120&y=922",
+    scores: [
+        "5",
+        "2",
+        "3",
+        "4",
+        "3",
+        "3",
+        "5",
+        "3",
+        "1",
+        "2"
+    ]
+},
+{
+    name:"Generic White Gal",
+    photo: "https://thoughtcatalog.files.wordpress.com/2017/02/31370639421_e1ce86e58a_o.jpg?w=1140&h=760&resize=1140,760&quality=95&strip=all&crop=1",
+    scores: [
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5"
+
+
+]
+},
+{
+    name:"Generic White Guy",
+    photo: "https://i.pinimg.com/236x/53/bf/b8/53bfb81027e4b080f49614f5d30d2798--beautiful-boys-pretty-boys.jpg",
+    scores: [
+        "1",
+        "1",
+        "1",
+        "1",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2"
+
+
+]
+},
+{
+    name:"Secret Agent Lady",
+    photo: "https://cdn.britannica.com/59/182359-050-C6F38CA3/Scarlett-Johansson-Natasha-Romanoff-Avengers-Age-of.jpg",
+    scores: [
+        "3",
+        "2",
+        "3",
+        "3",
+        "2",
+        "3",
+        "3",
+        "3",
+        "3",
+        "2"
+
+
+]
+},
+];
+module.exports= friends;
---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require ("express");
+var app = express ();
+var path = require ("path");
+var bodyParser = require ("body-parser");
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json({type: 'application/**json'}));
+
+app.use(bodyParser.raw({type: 'application/vnd.custom-type'}));
+
+app.use(bodyParser.text({type: 'text/html'}));
+
+require("../app/routing/apiRoutes.js")(app);
+require("../app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function (){
+    console.log("Friend Finding on PORT: " + PORT);
+    });
+
+
